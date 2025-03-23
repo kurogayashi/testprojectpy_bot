a41bcdba6da1d8fd1ead61cf55427a5660b821d2
kurogayashi <kurogayashi@mail.ru>
2025-03-23 22:47:05 +0300

    added simple bot functional

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("hello world")
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+
+logging.basicConfig(level=logging.INFO)
+bot = Bot(token='7261384228:AAHW6ol5DsC8rdPaLCsds6uAZpPOQ2Az77E')
+dp = Dispatcher()
+
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    await message.answer("Hello!")
+
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
